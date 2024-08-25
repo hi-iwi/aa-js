00f22d11a6e08f314b92367c83282c6bb06595a5
Aario <root@localhost.localdomain>
2024-08-25 19:09:36 +0800

    feat:paths support url

--- src/b_paths.js
+++ src/b_paths.js
@@ -1,69 +1,30 @@
 class paths {
     name = 'aa-path'
-    // @proprerty {string} the directory of the, e.g. /a/b
+    /** @proprerty {string} the directory of the, e.g. /a/b */
     dir
-    // @property {string} the last element of path, e.g.  / ==> /;   /a/b => b
+    /** @property {string} the last element of path, e.g.  / ==> /;   /a/b => b */
     base
-    // @property {string} the extension of path, e.g. ".js"
+    /** @property {string} the extension of path, e.g. ".js" */
     ext
-    // @property {string} the file name of path, e.g.  "a/b/c.jpg" => c
+    /** @property {string} the file name of path, e.g.  "a/b/c.jpg" => c */
     filename
-
+    /** @property {string} the href.origin, e.g. https://luexu.com */
+    origin
 
     /**
      * @param {string} path
      */
     init(path) {
-        path = paths.clean(path)
-        if (!path) {
-            this.dir = ''
-            this.base = ''
-            this.ext = ''
-            this.filename = ''
-            return
-        }
-        const a = path.split('/')
-        this.dir = a.slice(0, a.length - 1).join('/') || '/'
-        this.base = a[a.length - 1] || '/'
-        this.filename = this.base.replace(/\.[a-zA-Z\d]+$/g, '')
-        this.ext = this.base.replace(this.filename, '')
-    }
-
-    /**
-     * @param {string} path
-     */
-    constructor(path) {
-        this.init(path)
-    }
-
-    // Report whether the path is absolute
-    isAbs() {
-        return this.dir[0] === '/'
-    }
-
-    toSlash(separator = '\\') {
-        return this.toString().replace(/\//g, separator)
-    }
-
-    toString() {
-        return paths.join(this.dir, this.base)
-    }
-
-    /**
-     * Clean a path to get the shortest path name equivalent to path by purely lexical processing.
-     * @param {string} path
-     * @example
-     *  .clean("/../a/c/")  ===>  /a/c
-     *  .clean("a/b/c/..")  ===> a/b
-     *  .clean("./a/b/c/..")  ===> ./a/b
-     *  .clean("/./a/b/c/..")  ===> /a/b
-     *  .clean("/../a//b/c/d/e/../../../f/.//./g/.//.///./../.././i/./../.")  ===> /a/b
-     *  .clean("../a//b/c/d/e/../../../f/.//./g/.//.///./../.././i/./../.")  ===> ../a/b
-     */
-    static clean(path) {
         if (!path || path === '.') {
             return ''
         }
+        let origin = ''
+        let match = path.match(/^\w+:\/\/[^\/]+\/?/)
+        if (match) {
+            origin = match[0].replace(/\/$/, '')
+            path = path.substring(origin.length)
+        }
+
         // Replace multiple slashes with a single slash.      e.g. "a//b/c" ===> "a/b/c
         path = path.replace(/\/+/g, '/')
         // Eliminate each . path name element (the current directory).
@@ -81,7 +42,66 @@ class paths {
         }
         path = path.replace(/^\/..\//g, '/')  // root 的 /../  属于特殊情况，直接返回 /
         // Eliminate .. elements that begin a rooted path: that is, replace "/.." by "/" at the beginning of a path.
-        return path !== '/' ? path.replace(/\/$/, '') : path
+        if (path !== '/') {
+            path = path.replace(/\/$/, '')
+        }
+
+        this.origin = origin
+        if (!path || path === '/') {
+            this.dir = ''
+            this.base = path
+            this.ext = ''
+            this.filename = ''
+            return
+        }
+        const a = path.split('/')
+        this.dir = a.slice(0, a.length - 1).join('/') || '/'
+        this.base = a[a.length - 1] || '/'
+
+        let b = this.base.split('.')
+        if (b.length === 1) {
+            this.ext = ''
+            this.filename = this.base
+        } else {
+            this.ext = b[b.length - 1]
+            this.filename = b.slice(0, b.length - 1).join('.')
+        }
+    }
+
+    /**
+     * @param {string} path
+     */
+    constructor(path) {
+        this.init(path)
+    }
+
+
+    // Report whether the path is absolute
+    isAbs() {
+        return this.dir[0] === '/'
+    }
+
+    toSlash(separator = '\\') {
+        return this.toString().replace(/\//g, separator)
+    }
+
+    toString() {
+        return this.origin + paths.join(this.dir, this.base)
+    }
+
+    /**
+     * Clean a path to get the shortest path name equivalent to path by purely lexical processing.
+     * @param {string} path
+     * @example
+     *  .clean("/../a/c/")  ===>  /a/c
+     *  .clean("a/b/c/..")  ===> a/b
+     *  .clean("./a/b/c/..")  ===> ./a/b
+     *  .clean("/./a/b/c/..")  ===> /a/b
+     *  .clean("/../a//b/c/d/e/../../../f/.//./g/.//.///./../.././i/./../.")  ===> /a/b
+     *  .clean("../a//b/c/d/e/../../../f/.//./g/.//.///./../.././i/./../.")  ===> ../a/b
+     */
+    static clean(path) {
+        return new paths(path).toString()
     }
 
     /**
@@ -94,7 +114,5 @@ class paths {
         let path = args.join('/')
         return paths.clean(path)
     }
-
-
 }
 
